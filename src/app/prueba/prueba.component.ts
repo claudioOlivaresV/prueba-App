d28f7f47ad71d3752fd0a3f2956df69551c09e8a
Claudio Olivares <caolivares.3it@bancochile.cl>
2022-12-14 15:12:36 -0300

    test

--- src/app/prueba/prueba.component.ts
+++ src/app/prueba/prueba.component.ts
@@ -14,7 +14,7 @@ export class PruebaComponent implements OnInit {
   }
 
   launchAppOnAppleDevice() {
-    const url = "bancochilebanconexion://";
+    const url = "banconexion://";
     const resp =  window.location.replace(url);
     console.log(resp);   
   }
