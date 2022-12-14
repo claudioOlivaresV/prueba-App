61ae360f5edfac9b7c95e01dafab45d9cb64252d
Claudio Olivares <caolivares.3it@bancochile.cl>
2022-12-14 15:40:12 -0300

    test nombreasdas

--- src/app/prueba/prueba.component.ts
+++ src/app/prueba/prueba.component.ts
@@ -14,7 +14,7 @@ export class PruebaComponent implements OnInit {
   }
 
   launchAppOnAppleDevice() {
-    const url = "banconexion://";
+    const url = "mibancoempresas://";
     const resp =  window.location.replace(url);
     console.log(resp);   
   }
