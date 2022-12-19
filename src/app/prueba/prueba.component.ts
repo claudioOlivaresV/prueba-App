c9fda23134482e7da8022d89cc70b5f6f1715cec
Claudio Olivares <caolivares.3it@bancochile.cl>
2022-12-19 12:49:42 -0300

    prueba test

--- src/app/prueba/prueba.component.ts
+++ src/app/prueba/prueba.component.ts
@@ -10,20 +10,30 @@ export class PruebaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.launchAppOnAppleDevice();
+    const resp = this.launchAppOnAppleDevice();
+    console.log(resp);
+    
+    
+    
+    
   }
 
   launchAppOnAppleDevice() {
     const url = "bancochilebanconexion://";
-    const resp =  window.location.replace(url);
-    console.log(resp);   
+    let respo = window.location.replace(url) + '';
+    
   }
   lunchMarket() {
     if (navigator.userAgent.toLowerCase().indexOf("android") > -1) {
-      window.location.href = 'market://details?id=cl.bancochile.mi_banco&hl=es_CL&gl=US';
-      setTimeout(() => {
-        window.location.href = 'https://appgallery.huawei.com/app/C106022449?sharePrepath=ag';        
-      }, 30);
+      const url = "market://details?id=id=cl.bancochile.mi_banco&hl=es_CL&gl=US";
+      let respo = window.location.replace(url) + '';
+      console.log(respo);
+      if(respo === 'undefined') {
+        setTimeout(() => {
+          window.location.href = 'https://appgallery.huawei.com/app/C106022449?sharePrepath=ag';        
+        }, 30);
+      }
+      
     }
     if (navigator.userAgent.toLowerCase().indexOf("iphone") > -1) {
       window.location.href = 'https://apps.apple.com/cl/app/mi-banco-chile/id1516872542';
