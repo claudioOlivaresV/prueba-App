4807a42cf3554fe8b53769c07052e309b5ec9159
Claudio Olivares <caolivares.3it@bancochile.cl>
2022-12-20 15:52:02 -0300

    asdas

--- src/app/prueba/prueba.component.ts
+++ src/app/prueba/prueba.component.ts
@@ -10,42 +10,46 @@ export class PruebaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const resp = this.launchAppOnAppleDevice().catch(()=>{
-      console.log('aqui')
-    });
-    console.log(resp);
-    
-    
-    
-    
+    // const resp = this.launchAppOnAppleDevice().catch(() => {
+    //   console.log('aqui')
+    // });
+    // console.log(resp);
+
+
+
+
   }
 
   async launchAppOnAppleDevice() {
     const url = "cl.bancochile.banconexion ://";
-    let respo = await fetch( window.location.href = url );
+    let respo = await fetch(window.location.href = url);
     return respo
   }
   openAndroid() {
-    const url = "market://details?id=cl.bancochile.mi_banco&hl=es_CL&gl=US";
-    window.location.href = url;
+    // const url = "market://details?id=cl.bancochile.mi_banco&hl=es_CL&gl=US";
+    // window.location.href = url;
     const urlHuawei = 'market://details?id=C106022449?sharePrepath=ag'
     window.location.href = urlHuawei;
 
 
 
-    
+
   }
   lunchMarket() {
     if (navigator.userAgent.toLowerCase().indexOf("android") > -1) {
-      this.openAndroid();
+      // this.openAndroid();
+      const url = "market://details?id=cl.bancochile.mi_banco&hl=es_CL&gl=US";
+      window.location.href = url;
+      const urlHuawei = 'market://details?id=C106022449?sharePrepath=ag'
+      window.location.href = urlHuawei;
       // this.openAndroid().then(() => {
       //   console.log('ok');
-        
+
       // }).catch(()=> {
       //   console.log('error');
-        
+
       // })
-      
+
       // const url = "market://details?id=cl.bancochile.mi_banco&hl=es_CL&gl=US";
       // const test = window.open = function() { 
       //   if(url) { 
@@ -55,7 +59,7 @@ export class PruebaComponent implements OnInit {
       // } 
 
       // console.log(test);
-      
+
       // let respo = window.open(url);
       // console.log(respo);
       // console.log(respo?.document.URL);
@@ -66,18 +70,18 @@ export class PruebaComponent implements OnInit {
       //   //     window.location.href = 'https://appgallery.huawei.com/app/C106022449?sharePrepath=ag';        
       //   //   }, 30);
       //   // // }
-        
+
 
       // }
-      
+
     }
     if (navigator.userAgent.toLowerCase().indexOf("iphone") > -1) {
       window.location.href = 'https://apps.apple.com/cl/app/mi-banco-chile/id1516872542';
     }
-    if(navigator.userAgent.match(/iPad|iPod/i) || navigator.userAgent.includes("Mac") && "ontouchend" in document) {
-      window.location.href = 'https://apps.apple.com/cl/app/mi-banco-chile/id1516872542';      
+    if (navigator.userAgent.match(/iPad|iPod/i) || navigator.userAgent.includes("Mac") && "ontouchend" in document) {
+      window.location.href = 'https://apps.apple.com/cl/app/mi-banco-chile/id1516872542';
     }
-    if(navigator.userAgent.toLowerCase().indexOf("harmonyos") > -1 ) {
+    if (navigator.userAgent.toLowerCase().indexOf("harmonyos") > -1) {
       window.location.href = 'https://appgallery.huawei.com/app/C106022449?sharePrepath=ag';
     }
   }
